868defe24a385e05b9f6da0ab39b775dfcf7c77e
Jonathan Julian <jonathan.m.julian@gmail.com>
2011-11-26 14:58:44 -0500

    do not reload from server on app wake

--- Harbor/HarborAppDelegate.m
+++ Harbor/HarborAppDelegate.m
@@ -50,7 +50,6 @@
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
-    [self.viewController refresh];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
